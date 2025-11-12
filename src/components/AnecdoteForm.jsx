20a0c58601b3716c287b113f7b3e92c81f5872fc
vrahkonenn <vrahkonen02@gmail.com>
2025-11-12 16:43:59 +0200

    added json backend server

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,15 +1,19 @@
 import { useDispatch } from "react-redux"
 import { createAnecdote } from "../reducers/anecdoteReducer"
 import { setNotification } from "../reducers/notificationReducer"
+import anecdoteService from "../services/anecdote"
 
 const AnecdoteForm = () => {
 	const dispatch = useDispatch()
 
-	const addAnecdote = (event) => {
+	const addAnecdote = async (event) => {
 		event.preventDefault()
 		const content = event.target.content.value
 		event.target.content.value = ''
-		dispatch(createAnecdote(content))
+
+		const newAnecdote = await anecdoteService.createNew(content)
+		dispatch(createAnecdote(newAnecdote))
+		
 		dispatch(setNotification(`You created anecdote: "${content}"`))
 		setTimeout(() => {
 			dispatch(setNotification(''))
---
+++ src/services/anecdote.js
@@ -0,0 +1,29 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+    const response = await fetch(baseUrl)
+
+    if(!response.ok) {
+        throw new Error('Failed to fetch anecdotes')
+    }
+
+    return await response.json()
+}
+
+const createNew = async (content) => {
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ content, votes: 0 })
+    }
+
+    const response = await fetch(baseUrl, options)
+
+    if (!response.ok) {
+        throw new Error('Failed to create anecdote')
+    }
+
+    return await response.json()
+}
+
+export default { getAll, createNew }
